183688767eb252e7d9bc740d2316e198073feed3
Iana K <ianakostyk@gmail.com>
2025-04-19 22:15:23 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,32 +1,9 @@
-import { Schema, model } from 'mongoose';
+import Contact from '../db/models/contact.js';
 
-const contactSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-    },
-    isFavourite: {
-      type: Boolean,
-      default: false,
-    },
-    contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      required: true,
-      default: 'personal',
-    },
-  },
-  {
-    timestamps: true,
-  },
-);
+export const getAllContacts = async () => {
+  return await Contact.find({});
+};
 
-export default model('Contact', contactSchema);
+export const getContactById = async (contactId) => {
+  return await Contact.findById(contactId);
+};
