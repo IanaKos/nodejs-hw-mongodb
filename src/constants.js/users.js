45164495d00cdd002e892c8fa74f5c48f23081be
Iana K <ianakostyk@gmail.com>
2025-05-07 13:44:05 +0200

    upd

--- src/constants.js/users.js
+++ src/constants.js/users.js
@@ -1,12 +1,3 @@
-export const SORT_ORDER = {
-    ASC: 'asc',
-    DESC: 'desc',
-  };
-  
-  export const FIFTEEN_MINUTES = 15 * 60 * 1000;
-  export const ONE_DAY = 24 * 60 * 60 * 1000;
-  
-  export const ROLES = {
-    TEACHER: 'teacher',
-    PARENT: 'parent',
-  };
+export const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+export const accessTokenLifetime = 1000 * 60 * 15;
+export const refreshTokenLifeTime = 1000 * 60 * 60 * 24 * 30;
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,12 +1,12 @@
 import Joi from 'joi';
-
-export const registrationSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+import { emailRegexp } from '../constants/users.js';
+export const authRegisterSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
 });
 
-export const loginUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+export const authLoginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
 });
